6722551ed90d8ff0c763e8d3fc11d650caa62a0c
jinbo18101206396 <46548734+jinbo18101206396@users.noreply.github.com>
2022-01-28 20:12:36 +0800

    interface

--- src/main/java/cn/stylefeng/guns/modular/controller/QueryController.java
+++ src/main/java/cn/stylefeng/guns/modular/controller/QueryController.java
@@ -24,7 +24,6 @@
  */
 package cn.stylefeng.guns.modular.controller;
 
-import cn.stylefeng.guns.modular.model.request.ProofRequest;
 import cn.stylefeng.guns.modular.model.request.QueryRequest;
 import cn.stylefeng.guns.modular.service.QueryService;
 import cn.stylefeng.roses.kernel.rule.pojo.response.ResponseData;
@@ -55,7 +54,7 @@ public class QueryController {
      * @author 金波
      * @date 2022/01/21
      */
-    @PostResource(name = "保存质证信息", path = "/proof/add")
+    @PostResource(name = "保存质证信息", path = "/query/add")
     public ResponseData add(@RequestBody @Validated(QueryRequest.add.class) QueryRequest queryRequest) {
         queryService.add(queryRequest);
         return new SuccessResponseData();
@@ -67,7 +66,7 @@ public class QueryController {
      * @author 金波
      * @date 2022/01/21
      */
-    @PostResource(name = "通过ID更新质证信息", path = "/proof/edit")
+    @PostResource(name = "通过ID更新质证信息", path = "/query/edit")
     public ResponseData editById(@RequestBody @Validated(QueryRequest.edit.class) QueryRequest queryRequest) {
         queryService.updateById(queryRequest);
         return new SuccessResponseData();
@@ -79,7 +78,7 @@ public class QueryController {
      * @author 金波
      * @date 2022/01/21
      */
-    @PostResource(name = "通过案号和名称更新质证信息", path = "/proof/edit/number/name")
+    @PostResource(name = "通过案号和名称更新质证信息", path = "/query/edit/number/name")
     public ResponseData editByNumberAndName(@RequestBody @Validated(QueryRequest.edit.class) QueryRequest queryRequest) {
         queryService.updateByNumberAndName(queryRequest);
         return new SuccessResponseData();
