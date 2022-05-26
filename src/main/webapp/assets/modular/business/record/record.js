f07eb86f598fc23225cdef2388eab7bd0dbd2dc9
Victimmm <1325844611@qq.com>
2022-05-26 10:25:20 +0800

    添加数据回显

--- src/main/webapp/assets/modular/business/record/record.js
+++ src/main/webapp/assets/modular/business/record/record.js
@@ -89,11 +89,143 @@ layui.use(['table', 'HttpRequest', 'func', 'form','laydate'], function () {
      * @param data 点击按钮时候的行数据
      */
     Record.onDetailItem = function (data) {
+        //
+        $.get(
+            "/record/detail",
+            {courtNumber: data.courtNumber},
+            function(data) {
+                let wholeItem = JSON.parse(data.data)
+                let myLocalStorage={}
+                if (wholeItem != null) {
+                    //组织数据
+                    if ("basicInfo" in wholeItem)
+                        myLocalStorage["BasicInfo"] = wholeItem.basicInfo
+                    //基本信息陈述
+                    if ("stateInfo" in wholeItem)
+                        myLocalStorage["BasicState"] = wholeItem.stateInfo
 
+                    //原告数据
+                    if ("accuserInfo" in wholeItem && wholeItem.accuserInfo.length > 0) {
+                        let PlaintiffItems = []
+                        for (var j = 0; j < wholeItem.accuserInfo.length; j++) {
+                            PlaintiffItems.push(wholeItem.accuserInfo[j])
+                            if (wholeItem.accuserInfo[j].accuser_type == "1"){
+                                PlaintiffItems[j].accuser_fullname = wholeItem.accuserInfo[j].accuser
+                                PlaintiffItems[j].accuser=""
+                            }
+                        }
+                        myLocalStorage["PlaintiffItems"] = PlaintiffItems
+                    }
+
+                    //被告数据
+                    if ("defendantInfo" in wholeItem && wholeItem.defendantInfo.length > 0) {
+                        let  DefendantItems= []
+                        for (j = 0; j < wholeItem.defendantInfo.length; j++) {
+                            DefendantItems.push(wholeItem.defendantInfo[j])
+                        }
+                        myLocalStorage["DefendantItems"] = DefendantItems
+                    }
+
+                    if ("rightInfo" in wholeItem) {
+                        let rightInfo = wholeItem.rightInfo
+                        myLocalStorage["rightInfo"] = rightInfo
+                    }
+
+                    //法庭调查数据，包含原被告举证表，法庭调查表三个表
+                    if ("courtInvestigate" in wholeItem) {
+                        let courtTemp= wholeItem.courtInvestigate // 临时存储数据
+                        let courtInves={
+                            accuser_claim_item: courtTemp.accuser_claim_item,// 原告诉讼请求
+                            accuser_claim_fact_reason:courtTemp.accuser_claim_fact_reason ,// 原告诉讼请求的事实及理由
+                            is_counterclaim: courtTemp.is_counterclaim,
+                            defendant_reply: courtTemp.defendant_reply,
+                            counterclaim_accuser_claim_item: courtTemp.counterclaim_accuser_claim_item,
+                            counterclaim_accuser_fact_reason: courtTemp.counterclaim_accuser_fact_reason,
+                            counterclaim_defendant_reply:courtTemp.counterclaim_defendant_reply,
+                            counterclaim_defendant_today_is_reply: courtTemp.counterclaim_defendant_today_is_reply,
+                        }
+                        myLocalStorage["CourtInves"] = courtInves
+
+                        let accShowInfo={
+                            //第一个动态生成的json accuser_evidence 包含以下3个信息
+                            accuser_evidence: courtTemp.accuser_evidence,
+                            accuser_evidence_fact_reason: courtTemp.accuser_evidence_fact_reason, //事实和理由(原告举证)
+                            defendant_query: courtTemp.defendant_query,
+                        }
+                        myLocalStorage["accShowInfo"] = accShowInfo
+
+                        let defendantShowInfo={
+                            //第一个动态生成的json defendant_evidence 包含以下3个信息
+                            defendant_evidence: courtTemp.defendant_evidence,
+                            defendant_evidence_fact_reason: courtTemp.defendant_evidence_fact_reason,   //事实和理由(被告举证)
+                            //第二个动态生成的json accuser_query 包含以下6个信息
+                            accuser_query: courtTemp.accuser_query,
+//其他被告质证环节
+                            other_defendant_query: courtTemp.other_defendant_query,
+
+                            //反诉后的答辩情况
+                            //反诉后第一个部分 反诉被告（原告）进行举证
+                            counterclaim_defendant_evidence: courtTemp.counterclaim_defendant_evidence,
+                            counterclaim_defendant_evidence_fact_reason: courtTemp.counterclaim_defendant_evidence_fact_reason ,  //事实和理由(反诉被告提出，事实和理由 不是动态添加的)
+                            //反诉后第二个动态生成的部分 反诉原告质证
+                            counterclaim_accuser_query: courtTemp.counterclaim_accuser_query,
+                            other_counterclaim_defendant_query: courtTemp.other_counterclaim_defendant_query,
+                            counterclaim_accuser_fact_reason:courtTemp.counterclaim_accuser_fact_reason, //反诉原告的事实与理由
+                            //反诉后第三个生成部分 反诉原告 (原告) 进行举证
+                            counterclaim_accuser_evidence:courtTemp.counterclaim_accuser_evidence,
+                            //反诉后第四个生成部分 反诉被告 (原告) 进行质证
+                            counterclaim_defendent_query: courtTemp.counterclaim_defendent_query,
+                        }
+                        myLocalStorage["defendantShowInfo"] = defendantShowInfo
+                    }
+
+
+                    //法庭询问表
+                    // let inquiryInfo=[]
+                    if ("inquiryInfo" in wholeItem) {
+                        let inquiryInfoItem = wholeItem.inquiryInfo
+                        myLocalStorage["inquiryInfo"]={}
+                        myLocalStorage["inquiryInfo"]["inquiry_info"] = inquiryInfoItem
+                    }
+
+                    //法庭辩论表
+                    if ("argueInfo" in wholeItem) {
+                        let argueInfo = wholeItem.argueInfo
+                        myLocalStorage["argueInfo"] = argueInfo
+                    }
+
+                    //最后陈述表
+                    if ("finalStatementInfo" in wholeItem) {
+                        let finalStatementInfoItem = wholeItem.finalStatementInfo
+                        myLocalStorage["finalStatementInfo"]={}
+                        myLocalStorage["finalStatementInfo"]["final_statement_info"] = finalStatementInfoItem
+                    }
+
+                    //是否调解表
+                    let mediateInfo = {}
+                    if ("mediateInfo" in wholeItem) {
+                        mediateInfo = wholeItem.mediateInfo
+                        myLocalStorage["mediateInfo"] = mediateInfo
+                    }
+
+                    //电子文书送达表
+                    if ("deliveryInfo" in wholeItem) {
+                        let deliveryInfoItem = wholeItem.deliveryInfo
+                        myLocalStorage["deliveryInfo"]={}
+                        myLocalStorage["deliveryInfo"]["delivery_info"] = deliveryInfoItem
+                    }
+
+                    localStorage.setItem( wholeItem.basicInfo.court_number,JSON.stringify(myLocalStorage))
+                    //获取笔录信息，传到前台
+                    $(location).attr('href', '/view/record/detail'+"/?CourtNum="+wholeItem.basicInfo.court_number);
+                }
+            },
+            "json"
+        );
 
 
         //获取笔录信息，传到前台
-        $(location).attr('href', '/view/record/detail');
+        // $(location).attr('href', '/view/record/detail');
     };
 
     // 渲染表格
@@ -132,9 +264,7 @@ layui.use(['table', 'HttpRequest', 'func', 'form','laydate'], function () {
         } else if (layEvent === 'delete') {
             Record.onDeleteItem(data);
         }else if (layEvent == 'detail'){
-
-            alert(data)
-
+            // console.log(data.courtNumber)
             Record.onDetailItem(data);
         }
     });
