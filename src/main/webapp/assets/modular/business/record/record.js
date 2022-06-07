46428dd6e54adb19ee0c6076d1ba606771064067
Victimmm <1325844611@qq.com>
2022-06-07 22:37:16 +0800

    添加 案号重复处理，修改质证显示、居中对齐等bug

--- src/main/webapp/assets/modular/business/record/record.js
+++ src/main/webapp/assets/modular/business/record/record.js
@@ -107,7 +107,7 @@ layui.use(['table', 'HttpRequest', 'func', 'form', 'laydate'], function () {
                         myLocalStorage["CourtInves"] = courtInves
 
                         courtTemp.defendant_query.forEach(e => {
-                            e.defendant = Array.isArray(e.defendant)  ?e.defendant.split("**"): [] , e.evidence = Array.isArray(e.evidence)? e.evidence.split("**"):[]
+                            e.defendant = e.defendant  ?e.defendant.split("**"): [] , e.evidence = e.evidence? e.evidence.split("**"):[]
                         })
                         let accuserShowInfo = {
                             //第一个动态生成的json accuser_evidence 包含以下3个信息
@@ -119,19 +119,19 @@ layui.use(['table', 'HttpRequest', 'func', 'form', 'laydate'], function () {
                         myLocalStorage["accuserShowInfo"] = accuserShowInfo
 
                         courtTemp.accuser_query.forEach(e => {
-                            e.accuser =(e.accuser)?e.accuser.split("**"):[], e.evidence = Array.isArray(e.evidence)?e.evidence.split("**"):[]
+                            e.accuser =e.accuser?e.accuser.split("**"):[], e.evidence = e.evidence?e.evidence.split("**"):[]
                         })
                         courtTemp.other_defendant_query.forEach(e => {
-                            e.defendant =Array.isArray(e.defendant)?e.defendant.split("**"):[], e.evidence = Array.isArray(e.evidence)?e.evidence.split("**"):[]
+                            e.defendant =e.defendant?e.defendant.split("**"):[], e.evidence = e.evidence?e.evidence.split("**"):[]
                         })
                         courtTemp.counterclaim_accuser_query.forEach(e => {
-                            e.counterclaim_accuser = Array.isArray(e.counterclaim_accuser)?e.counterclaim_accuser.split("**"):[],e.evidence = Array.isArray(e.evidence)?e.evidence.split("**"):[]
+                            e.counterclaim_accuser = e.counterclaim_accuser?e.counterclaim_accuser.split("**"):[],e.evidence = e.evidence?e.evidence.split("**"):[]
                         })
                         courtTemp.other_counterclaim_defendant_query.forEach(e => {
-                            e.other_counterclaim_defendant = Array.isArray(e.other_counterclaim_defendant)?e.other_counterclaim_defendant.split("**"):[], e.evidence = Array.isArray(e.evidence)?e.evidence.split("**"):[]
+                            e.other_counterclaim_defendant = e.other_counterclaim_defendant?e.other_counterclaim_defendant.split("**"):[], e.evidence =e.evidence?e.evidence.split("**"):[]
                         })
                         courtTemp.counterclaim_defendant_query.forEach(e => {
-                            e.counterclaim_defendant =Array.isArray(e.counterclaim_defendant)? e.counterclaim_defendant.split("**"):[], e.evidence = Array.isArray(e.evidence)?e.evidence.split("**"):[]
+                            e.counterclaim_defendant =e.counterclaim_defendant? e.counterclaim_defendant.split("**"):[], e.evidence = e.evidence?e.evidence.split("**"):[]
                         })
 
                         let defendantShowInfo = {
